44d0de72a185632ec2a69b2eeadfc9cb9faeceef
nishin_soba <townpe3@gmail.com>
2022-02-20 21:08:15 +0900

    JsonPropertyName設定（その2）

--- GetRemoInfo/RemoBatchData.cs
+++ GetRemoInfo/RemoBatchData.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace GetRemoInfo
 {
@@ -9,13 +10,17 @@ namespace GetRemoInfo
     [DynamoDBTable("remo_batch_data")]
     public class RemoBatchData
     {
+        [JsonPropertyName("datetime")]
         public string DateTime { get; set; }
-
+        [JsonPropertyName("is_using_aircon")]
         public int IsUsingAircon { get; set; }
+        [JsonPropertyName("outdoor_temperature")]
         public double OutdoorTemperature { get; set; }
+        [JsonPropertyName("room_temperature")]
         public double RoomTemperature { get; set; }
-
+        [JsonPropertyName("aircon_mode")]
         public string AirconMode { get; set; }
+        [JsonPropertyName("aircon_temperature")]
         public double AirconTemperature { get; set; }
        
     }
